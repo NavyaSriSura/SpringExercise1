d5c3e3aac12ee67736a1c569b8bca985a80254da
NavyaSriSura <navyasura98@gmail.com>
2019-07-16 14:12:00 +0530

    first commit

---
+++ src/main/java/com/stackroute/Controller/UserController.java
@@ -0,0 +1,17 @@
+package com.stackroute.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController
+{
+    @RequestMapping("/")
+    public String greet(Model model){
+        User u = new User("navya");
+        model.addAttribute("name",u.getName());
+        return "index";
+    }
+
+}
